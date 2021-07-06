e8d5ee9aa0c31c62cb2df2ca42153327f57acac1
nefynbait <63865577+nefynbait@users.noreply.github.com>
2021-07-06 20:40:46 +0530

    Add files via upload

---
+++ PROJ 3.1/PROJ 3.1/CODE/simu1.m
@@ -0,0 +1,18 @@
+function simulate=simu1(thetaini,Mlist,Glist,Slist,g)
+  [row col]=size(thetaini);
+  dtheta=zeros(row,1); 
+  tau=zeros(row,1);
+  ftip=zeros(6,1);
+  ddtheta=[];
+  simulate=zeros(301,row);
+  simulate(1,:)=thetaini';
+  r=2;
+  for t=0:1/100:3
+    ddtheta=ForwardDynamics(thetaini,dtheta,tau,g,ftip,Mlist,Glist,Slist);
+    thetaini=thetaini+dtheta*(1/100)+(1/100)*(1/100)*ddtheta/2; %(finding the position after one time step)
+    dtheta=dtheta+(1/100)*ddtheta;  %(finding the velocity after one time step)
+    simulate(r,:)=thetaini'; %(saving the postion in simulate var)
+    r=r+1;
+  endfor
+  csvwrite('output1.csv',simulate);
+endfunction
---
+++ PROJ 3.1/PROJ 3.1/CODE/simu2.m
@@ -0,0 +1,18 @@
+function simulate=simu2(thetaini,Mlist,Glist,Slist,g)
+  [row col]=size(thetaini);
+  dtheta=zeros(row,1); 
+  tau=zeros(row,1);
+  ftip=zeros(6,1);
+  ddtheta=[];
+  simulate=zeros(501,row);
+  simulate(1,:)=thetaini';
+  r=2;
+  for t=0:1/100:3
+    ddtheta=ForwardDynamics(thetaini,dtheta,tau,g,ftip,Mlist,Glist,Slist);
+    thetaini=thetaini+dtheta*(1/100)+(1/100)*(1/100)*ddtheta/2; %(finding the position after one time step)
+    dtheta=dtheta+(1/100)*ddtheta;  %(finding the velocity after one time step)
+    simulate(r,:)=thetaini'; %(saving the postion in simulate var)
+    r=r+1;
+  endfor
+  csvwrite('output2.csv',simulate);
+endfunction
